ecb2773e1adb31697a743aeacf3c147a6fbe73bc
Simon-Winiger <simon.winiger@bbzbl-it.ch>
2023-12-26 14:47:27 +0100

    JavaScript-Datei

---
+++ projektarbeit.js
@@ -0,0 +1,8 @@
+const ApiBerufe = "http://sandbox.gibm.ch/berufe.php"; //Example could look like http://sandbox.gibm.ch/berufe.php
+const ApiKlassen = "http://sandbox.gibm.ch/klassen.php"; //Example could look like http://sandbox.gibm.ch/klassen.php?beruf_id=10
+const ApiTafel = "http://sandbox.gibm.ch/tafel.php"; //Example could look like http://sandbox.gibm.ch/tafel.php?klasse_id=2887489
+
+StartseiteButton.addEventListener('mouseover', () => {
+    // Change the button's background color
+    StartseiteButton.style.backgroundColor = 'blue';
+  });
